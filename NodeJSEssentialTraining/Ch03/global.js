3af0e84aee22224e571aaa55c433950a91d1cb86
Dian Aditya Nugraha <dian.aditya.n@gmail.com>
2018-10-23 08:13:04 +0700

    Completing Chapter 3

---
+++ NodeJSEssentialTraining/Ch03/global.js
@@ -0,0 +1,9 @@
+var path = require("path");
+//var hello = "Hello World from Node.js";
+// global.console.log("Hello") --EQUAL--> console.log("Hello")
+// console.log(global.hello) --RESULT--> undefined
+// var justNode = hello.slice(5);
+
+console.log(`Rock on World from ${path.basename(__filename)}`)
+// console.log(__dirname);
+// console.log(__filename);
